b54406f99b246a2e1b40b15908ef1ae07401f21e
twoo7 <62158470+twoo7@users.noreply.github.com>
2020-03-14 21:35:57 -0700

    deleted my q1 work/Update Q3.py

--- q3.py
+++ q3.py
@@ -4,50 +4,6 @@ import geopandas as gpd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-"""
-def Question1(data):
-    data = data[:, ['attributes.Music.dj', 'attributes.Music.playlist',
-                'attributes.Music.video', 'attributes.Music.karaoke',
-                    'attributes.Music.background_music',
-                    'attributes.Music.live', 'stars']]
-    data.fillna('FALSE')
-    dj = data[:, 'attributes.Music.dj', 'stars']
-    playlist = data[:, 'attributes.Music.playlist', 'stars']
-    video = data[:, 'attributes.Music.video', 'stars']
-    karaoke = data[:, 'attributes.Music.karaoke', 'stars']
-    background = data[:, 'attributes.Music.background_music']
-    live = data[:, 'attributes.Music.live']
-    stars = data[:, 'stars']
-    djModel = learn(dj, stars)
-    playlistModel = learn(playlist, stars)
-    videoModel = learn(video, stars)
-    karaokeModel = learn(karaoke, stars)
-    backgroundModel = learn(background, stars)
-    liveModel = learn(live, stars)
-    djModel['type'] = 'dj'
-    playlistModel['type'] = 'playlist'
-    videoModel['type'] = 'video'
-    karaokeModel['type'] = 'karaoke'
-    backgroundModel['type'] = 'background'
-    liveModel['type'] = 'live'
-
-    final = pd.concat([djModel, playlistModel, videoModel, karaokeModel,
-    backgroundModel, liveModel])
-    print(final)
-
-
-def learn(data, stars):
-    data.get_dummies()
-    x_train, x_test, y_train, y_test = train_test_split(data, stars,
-                                                        test_size=0.2)
-    model = DecisionTreeClassifier()
-    model.fit(x_train, y_train)
-    y_test_predict = model.predict(x_test)
-    print(accuracy_score(y_test, y_test_predict))
-    return model
-"""
-
-
 def Question3(data, geodata):
     data = data.loc[:, ['state', 'stars', 'categories']]
     data = data.dropna(subset=["categories"], how="all")
